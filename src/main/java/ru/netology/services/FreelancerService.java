e75d700b7eb59788e609c2fcd0bf54b1359c874c
Artemius <cpt.morgan0510@gmail.com>
2023-06-07 17:20:08 +0300

    First Commit

---
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -0,0 +1,22 @@
+package ru.netology.services;
+
+public class FreelancerService {
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+
+        int monthsRest = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                // отдыхаем
+                money -= expenses;
+                money /= 3;
+                monthsRest++;
+            } else {
+                // работаем
+                money += income;
+                money -= expenses;
+            }
+        }
+        return monthsRest;
+    }
+}
